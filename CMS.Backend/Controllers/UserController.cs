accca5c2490602e82a612e684b0b663bdddeeb15
MY NHU <tranthimynhu17@icloud.com>
2026-05-28 15:01:04 +0700

    hoàn thành buổi 6

--- CMS.Backend/Controllers/UserController.cs
+++ CMS.Backend/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CMS.Backend.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Admin")]
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _context;
